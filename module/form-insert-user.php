95822b52217a0e8b4de6d3a41cb921e3d86cbc83
danngyw <danng@youngworld.vn>
2016-10-03 15:16:02 +0700

    add user

--- module/form-insert-user.php
+++ module/form-insert-user.php
@@ -1,3 +1,39 @@
-<form action="#">
-	<
+<form action="#"  class="form-horizontal">
+	<div class="form-group row">
+	  <label for="example-text-input" class="col-xs-2 col-form-label">Họ</label>
+	  <div class="col-xs-10">
+	    <input class="form-control" type="text" name="ho" value="Artisanal kale" id="example-text-input">
+	  </div>
+	</div>
+
+	<div class="form-group row">
+	  <label for="example-tel-input" class="col-xs-2 col-form-label">Tên lót</label>
+	  <div class="col-xs-10">
+	    <input class="form-control" type="text" value="" name="ten_lot" id="example-text-input">
+	  </div>
+	</div>
+
+	<div class="form-group row">
+	  <label for="example-tel-input" class="col-xs-2 col-form-label">Tên </label>
+	  <div class="col-xs-10">
+	    <input class="form-control" type="text" value="" name="ten" id="example-text-input">
+	  </div>
+	</div>
+
+	<div class="form-group row">
+	  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Ngày sinh</label>
+	  <div class="col-xs-10">
+	    <input class="form-control" type="datetime-local" name="ngay_sinh" value="2011-08-19T13:45:00" id="example-datetime-local-input">
+	  </div>
+	</div>
+
+	<div class="form-group row">
+	  <div class="col-xs-10">
+	  &nbsp;
+	  </div>
+	  <div class="col-xs-2">
+	    	<button type="submit" class="btn btn-default btn-md"> Thêm mới</button>
+	  </div>
+	</div>
+
 </form>

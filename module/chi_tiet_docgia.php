a2e130a901bb9c7b23d8af46919b2624f02d0745
danngyw <danng@youngworld.vn>
2016-10-16 18:41:47 +0700

    update

--- module/chi_tiet_docgia.php
+++ module/chi_tiet_docgia.php
@@ -1,41 +1,106 @@
 <?php
 $ma_docgia = isset($_GET['id']) ? $_GET['id'] : 0;
 $docgia = TreEm::getInstance();
-echo "Mã độc giả : {$ma_docgia}".'<br />';
+$loaidocgia = 'Người lớn';
 $is_tre_em =  $docgia->isDocGiaTreEm($ma_docgia) ;
 
 if( $is_tre_em ) {
-	echo 'Loại độc giả: Trẻ em <br />';
-	$record = $docgia->getThongTinDocGia($ma_docgia);
-	echo 'Thông tin độc giả người lớn : <br />';
-	$ma_docgia = $docgia->maDocGiaNguoiLon($ma_docgia);
+	$loaidocgia = 'Trẻ em';
+	$treem 		= $docgia->getThongTinDocGia($ma_docgia);
+	$ma_docgia 	= $docgia->maDocGiaNguoiLon($ma_docgia);
 }
 $nguoilon = NguoiLon::getInstance();
 $record = $nguoilon->getThongTinDocGia($ma_docgia);
 
-
-if(!$is_tre_em){
-	echo 'Loại độc giả: Người lớn <br />';
-}
 $diachi = !empty($record['diachi']) ? $record['diachi'] : 'Chưa có địa chỉ';
 $hoten = !empty($record['hoten']) ? $record['hoten'] : 'Chưa nhập họ tên';
 $quan = !empty($record['quan']) ? $record['quan'] : 'Chưa nhập quận';
 $ngaysinh = !empty($record['ngaysinh']) ? $record['ngaysinh'] : 'Chưa nhập ngày sinh';
 $han_sd = !empty($record['han_sd']) ? $record['han_sd'] : 'Chưa nhập ngày sinh';
-// echo '<pre>';
-// var_dump($record);
-// echo '</pre>';
+
 $con_hsd = $record['con_hsd'];
+$han_sd_text  = ($con_hsd  == '1') ? 'Còn hạn sử dụng' :'<span class="error"> Hết hạn sử dụng </span>';
+?>
+<div class="form-group row">
+	<div class ="col-xs-12">
+  		<h3> Thông tin độc giả </h3>
+  	</div>
+</div>
 
-$han_sd_text =  ($con_hsd == 1)? 'Còn sạn sử dụng' : 'Thẻ hết hạn';
-echo 'Họ tên :'.$hoten .'<br />';
-echo 'Địa chỉ:'.$diachi.'<br />';
-echo 'Ngày sinh:'.$ngaysinh.'<br />';
-echo 'Ngày hết hạn:'.$han_sd.'<br />';
-echo 'Hạn sử dụng:'.$han_sd_text.'<br />';
-echo 'Quận:'.$quan.'<br />';
-echo 'Số sách đang mượn : '.soSachDangMuon($ma_docgia) .'<br />';
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Loại độc giả</label>
+  	<div class="col-xs-6">
+  		<?php echo $loaidocgia;?>
+  	</div>
+</div>
+<?php
+if($is_tre_em){ ?>
+	<div class="form-group row">
+	  	<label for="example-tel-input" class="col-xs-4 col-form-label">Họ tên </label>
+	  	<div class="col-xs-6">
+	  		<?php echo $treem['hoten'];?>
+	  	</div>
+	</div>
+	<div class="form-group row">
+	  	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày sinh</label>
+	  	<div class="col-xs-6">
+	  		<?php echo $treem['ngaysinh'];?>
+	  	</div>
+	</div>
 
+	<div class="form-group row">
+	  	<label for="example-tel-input" class="col-xs-12 col-form-label">Thông tin độc giả người lớn bảo hộ:</label>
+	</div>
+	<?php
+}
 ?>
 
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Tên độc giả</label>
+  	<div class="col-xs-6">
+  		<?php echo $hoten;?>
+  	</div>
+</div>
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Địa chỉ</label>
+  	<div class="col-xs-6">
+  		<?php echo $diachi;?>
+  	</div>
+</div>
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày sinh</label>
+  	<div class="col-xs-6">
+  		<?php echo $ngaysinh;?>
+  	</div>
+</div>
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày hết hạn</label>
+  	<div class="col-xs-6">
+  		<?php echo $han_sd;?>
+  	</div>
+</div>
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Tình Trạng</label>
+  	<div class="col-xs-6">
+  		<?php echo $han_sd_text;?>
+  	</div>
+</div>
 
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Địa chỉ</label>
+  	<div class="col-xs-6">
+  		<?php echo $diachi;?>
+  	</div>
+</div>
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Quận</label>
+  	<div class="col-xs-6">
+  		<?php echo $quan;?>
+  	</div>
+</div>
+<div class="form-group row">
+  	<label for="example-tel-input" class="col-xs-4 col-form-label">Số sách đang mượn</label>
+  	<div class="col-xs-6">
+  		<?php echo soSachDangMuon($ma_docgia);?>
+  	</div>
+</div>

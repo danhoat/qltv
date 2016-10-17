17d10679369cfc64b5c77cd33c5bbc78d2f52942
danngyw <danng@youngworld.vn>
2016-10-17 22:15:41 +0700

    upate

--- inc/connect.php
+++ inc/connect.php
@@ -1,22 +1,13 @@
 <?php
 	global $conn;
-	// $connect = mysql_connect("localhost","root","");
-	// if(!$connect)
-	// 	die("can not connect");
-	// $db = mysql_select_db("qltv", $connect);
-	// if(!$db){
-	// 	die(" Can not select db");
-	// }
-
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname = "qltv";
-
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
-}
+	}
 ?>

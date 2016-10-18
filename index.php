2c659fec4e2f7b2e0efc42f7e04b64d96ca683fc
danngyw <danng@youngworld.vn>
2016-10-18 21:25:01 +0700

    update

--- index.php
+++ index.php
@@ -25,14 +25,16 @@ if( isset($_POST['submit']) && $_POST['submit'] == 'login' ){
 ?>
 <head>
 	<link rel="stylesheet" type="text/css" href="assest/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css"> -->
+	<link rel="stylesheet" href="assest/css/bootstrap-select.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
+	<script src="assest/js/jquery.min.js"></script>
 	<script src="assest/js/bootstrap.min.js"></script>
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script>
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script> -->
+	<script src="assest/js/bootstrap-select.min.js"></script> -->
 
 	<meta charset ="utf-8"/>
 	<title><?php echo get_site_title();?></title>

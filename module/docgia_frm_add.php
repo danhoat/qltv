a814cdc827bdd1cabe734fc871cd768b6e1e1d2c
danngyw <danng@youngworld.vn>
2016-10-21 15:21:54 +0700

    update

--- module/docgia_frm_add.php
+++ module/docgia_frm_add.php
@@ -51,7 +51,6 @@ if(isset($_POST['submit'])){
                 <!-- /.panel-heading -->
                 <div class="panel-body">
                 <form action="#"  class="form-horizontal csf" method="POST" action="#">
-
 					<?php if( !empty( $error ) ){
 						echo '<div class="alert alert-danger">';
                         echo $error;
@@ -70,49 +69,43 @@ if(isset($_POST['submit'])){
 					    <input class="form-control required" type="text" required name="hoten" value="" placeholder="Nhập họ tên độc giả" id="example-text-input">
 					  </div>
 					</div>
+
+					<div class="form-group row">
+					  <label for="example-text-input" class="col-xs-2 col-form-label">Mã sinh viên</label>
+					  <div class="col-xs-10">
+					    <input class="form-control required" type="text" required name="masv" value="" placeholder="Nhập mã sinh viên" id="example-text-input">
+					  </div>
+					</div>
+
 					<div class="form-group row">
 					  <label for="example-tel-input" class="col-xs-2 col-form-label">Ngày sinh </label>
 					  <div class="col-xs-10"  data-provide="datepicker">
 					   <input class="form-control datepicker" data-format="yyyy-MM-dd hh:mm:ss" type="date" name="ngaysinh" id="ngaysinh" value="2016-12-16 00:00:00" >
 					  </div>
 					</div>
-					<div class="row-default">
-						<div class ="nguoilon <?php echo $frm_nguoilon;?>">
-							<div class="form-group row">
-							  <label for="example-tel-input" class="col-xs-2 col-form-label">Địa chỉ</label>
-							  <div class="col-xs-10">
-							    <input class="form-control is_required required" type="text" value="" placeholder="Nhập địa chỉ" name="diachi" id="example-text-input">
-							  </div>
-							</div>
 
-							<div class="form-group row">
-							  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Quận</label>
-							  <div class="col-xs-10">
-							    <?php danhSachQuan();?>
-							  </div>
-							</div>
-							<div class="form-group row">
-							  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Điện thoại</label>
-							  <div class="col-xs-10">
-							    <input class="form-control " type="text"  name="dienthoai" placeholder="Nhập điện thoại" />
-							  </div>
-							</div>
 
-							<div class="form-group row">
-							  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Hạn sử dụng</label>
-							  <div class="col-xs-10" >
-							    <input class="form-control datepicker" data-format="yyyy-MM-dd hh:mm:ss" type="date" name="han_sd" id="han_sd" value="2016-12-16 00:00:00" >
-							  </div>
-							</div>
-						</div>
-						<div class="treem <?php echo $frm_treem;?>">
-							<div class="form-group row">
-							  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Mã người lớn</label>
-							  <div class="col-xs-10">
-							     <?php danhSachNguoiLon();?>
-							  </div>
-							</div>
-						</div>
+					<div class="form-group row">
+					  <label for="example-tel-input" class="col-xs-2 col-form-label">Địa chỉ</label>
+					  <div class="col-xs-10">
+					    <input class="form-control is_required required" type="text" value="" placeholder="Nhập địa chỉ" name="diachi" id="example-text-input">
+					  </div>
+					</div>
+
+
+					<div class="form-group row">
+					  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Điện thoại</label>
+					  <div class="col-xs-10">
+					    <input class="form-control " type="text"  name="dienthoai" placeholder="Nhập điện thoại" />
+					  </div>
+					</div>
+
+					<div class="form-group row">
+					  <label for="example-datetime-local-input" class="col-xs-2 col-form-label">Hạn sử dụng</label>
+					  <div class="col-xs-10" >
+					    <input class="form-control datepicker" data-format="yyyy-MM-dd hh:mm:ss" type="date" name="han_sd" id="han_sd" value="2016-12-16 00:00:00" >
+					  </div>
+					</div>
 
 					</div>
 					<div class="form-group row">

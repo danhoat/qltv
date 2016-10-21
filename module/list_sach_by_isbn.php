50c97139866d5e7e94b1d76ed65d095505b5ead1
danngyw <danng@youngworld.vn>
2016-10-21 19:09:23 +0700

    fix html

--- module/list_sach_by_isbn.php
+++ module/list_sach_by_isbn.php
@@ -1,45 +1,48 @@
+<div id="page-wrapper">
 <?php
-$del_id     = isset($_GET['del']) ? $_GET['del'] : '';
-$cuonsach   = CuonSach::getInstance();
-if( !empty($del_id)){
-    $cuonsach->xoaCuonSach($del_id);
-}
-$isbn = isset($_GET['isbn']) ? $_GET['isbn'] : 0;
-$total = CuonSach::listBookByISBN($select_all = 1, $isbn);
-if( !empty($total) ){
-
-	$url = 'index.php?act=list_sach_by_isbn&';
-	$total_record = $total->num_rows;
-    $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
-    if(! is_numeric($current_page) || $current_page < 1){
-    	$current_page = 1;
+    $del_id     = isset($_GET['del']) ? $_GET['del'] : '';
+    $cuonsach   = CuonSach::getInstance();
+    if( !empty($del_id)){
+        $cuonsach->xoaCuonSach($del_id);
     }
-    $posts_per_page = 10;
-    $max_page = ceil($total_record/$posts_per_page);
-    if($current_page > $max_page){
-    	$current_page = $max_page;
-    }
-	$result = CuonSach::listBookByISBN($select_all = 0, $isbn, $posts_per_page, $current_page );
-    echo '<br />';
-    echo "<label>Đầu sách {$isbn} hiện có {$total_record} cuốn sách </label>";
-    echo '<br />';
-	echo '<table class="table table-bordered ">';
-	echo '<thead><tr><th> Mã</th> <th> Tựa sách </th><th> Tình trạng </td> <th> Tác vụ </th> </tr></thead>';
-	echo ' <tbody>';
-	while( $row = $result->fetch_assoc() ) {
-		$isbn 		   = $row['isbn'];
-        $ma_cuonsach   = $row['ma_cuonsach'];
-		$tinhtrang 	   = ($row['tinhtrang'] == 1)  ?  'Kho' :'Đang mượn <a href="index.php?act=chi_tiet_muon&mcs='.$ma_cuonsach.'">Chi tiết </a>';
-		echo '<tr>';
-        echo "<td> " . $row["ma_cuonsach"]. " </td><td> <a class='' href= '#'> " . limit_string( getTuaSachByISBN($isbn),30). "</a> </td><td>". $tinhtrang. "</td>";
+    $isbn = isset($_GET['isbn']) ? $_GET['isbn'] : 0;
+    $total = CuonSach::listBookByISBN($select_all = 1, $isbn);
+    if( !empty($total) ){
 
-        echo "<td><a  class='action'  href='index.php?act=frm_insert_book&id=".$ma_cuonsach."'>Cập nhật</a> &nbsp; <a href='{$url}page={$current_page}&del={$ma_cuonsach}' onclick ='return remove_tua_sach()'> Xóa</a>  ";
-        echo '</tr>';
-    }
-    echo '</tbody>';
-    echo '</table>';
+    	$url = 'index.php?act=list_sach_by_isbn&';
+    	$total_record = $total->num_rows;
+        $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+        if(! is_numeric($current_page) || $current_page < 1){
+        	$current_page = 1;
+        }
+        $posts_per_page = 10;
+        $max_page = ceil($total_record/$posts_per_page);
+        if($current_page > $max_page){
+        	$current_page = $max_page;
+        }
+    	$result = CuonSach::listBookByISBN($select_all = 0, $isbn, $posts_per_page, $current_page );
+        echo '<br />';
+        echo "<label>Đầu sách {$isbn} hiện có {$total_record} cuốn sách </label>";
+        echo '<br />';
+    	echo '<table class="table table-bordered ">';
+    	echo '<thead><tr><th> Mã</th> <th> Tựa sách </th><th> Tình trạng </td> <th> Tác vụ </th> </tr></thead>';
+    	echo ' <tbody>';
+    	while( $row = $result->fetch_assoc() ) {
+    		$isbn 		   = $row['isbn'];
+            $ma_cuonsach   = $row['ma_cuonsach'];
+    		$tinhtrang 	   = ($row['tinhtrang'] == 1)  ?  'Kho' :'Đang mượn <a href="index.php?act=chi_tiet_muon&mcs='.$ma_cuonsach.'">Chi tiết </a>';
+    		echo '<tr>';
+            echo "<td> " . $row["ma_cuonsach"]. " </td><td> <a class='' href= '#'> " . limit_string( getTuaSachByISBN($isbn),30). "</a> </td><td>". $tinhtrang. "</td>";
 
-    echo paginate( $posts_per_page, $current_page, $total_record,  $max_page, $url = 'index.php?act=list_sach');
-} else {
-    Echo 'Không có kết quả trả về';
-}
+            echo "<td><a  class='action'  href='index.php?act=frm_insert_book&id=".$ma_cuonsach."'>Cập nhật</a> &nbsp; <a href='{$url}page={$current_page}&del={$ma_cuonsach}' onclick ='return remove_tua_sach()'> Xóa</a>  ";
+            echo '</tr>';
+        }
+        echo '</tbody>';
+        echo '</table>';
+
+        echo paginate( $posts_per_page, $current_page, $total_record,  $max_page, $url = 'index.php?act=list_sach');
+    } else {
+        Echo 'Không có kết quả trả về';
+    }
+?>
+</div>

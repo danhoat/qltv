5e439e1f40db3d377a4108965d04b1c0ad91bd93
danngyw <danng@youngworld.vn>
2016-10-21 17:50:56 +0700

    update

--- module/chi_tiet_muon.php
+++ module/chi_tiet_muon.php
@@ -1,3 +1,4 @@
+<div id="page-wrapper">
 <?php
 	$ma_cuonsach = isset($_GET['mcs']) ? $_GET['mcs'] : 0;
 	$isbn = isset($_GET['isbn']) ? $_GET['isbn'] : 0;
@@ -13,54 +14,55 @@
   $docgia     = DocGia::getInstance()->getThongTinDocGia($ma_docgia);
 
 ?>
-<div class="form-group row">
-    <h3 class="col-xs-12 col-form-label"> Chi tiết mượn sách</h3>
-</div>
-<div class="form-group row">
-    <label for="example-tel-input" class="col-xs-4 col-form-label">Độc giả mượn</label>
-    <div class="col-xs-6">
-      <?php echo $docgia['hoten'] ;?><a href="index.php?act=chi_tiet_docgia&id=<?php echo $ma_docgia;?>"> Chi tiết</a>
-    </div>
-</div>
-<div class="form-group row">
-    <label for="example-tel-input" class="col-xs-4 col-form-label">Số sách đang mượn</label>
-    <div class="col-xs-3">
-      <?php echo soSachDangMuon($ma_docgia);?>
-    </div>
+  <div class="form-group row">
+      <h3 class="col-xs-12 col-form-label"> Chi tiết mượn sách</h3>
+  </div>
+  <div class="form-group row">
+      <label for="example-tel-input" class="col-xs-4 col-form-label">Độc giả mượn</label>
+      <div class="col-xs-6">
+        <?php echo $docgia['hoten'] ;?><a href="index.php?act=chi_tiet_docgia&id=<?php echo $ma_docgia;?>"> Chi tiết</a>
+      </div>
+  </div>
+  <div class="form-group row">
+      <label for="example-tel-input" class="col-xs-4 col-form-label">Số sách đang mượn</label>
+      <div class="col-xs-3">
+        <?php echo soSachDangMuon($ma_docgia);?>
+      </div>
 
-</div>
+  </div>
 
-<div class="form-group row">
-  	<label for="example-tel-input" class="col-xs-4 col-form-label">Tên Cuốn sách</label>
-  	<div class="col-xs-6">
-  		<?php echo $muon['bia'] ;?>
-  	</div>
-</div>
-<div class="form-group row">
-  	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày mượn</label>
-  	<div class="col-xs-6">
-  		<?php echo $muon['ngaygio_muon'] ;?>
-  	</div>
-</div>
-<div class="form-group row">
-  	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày hết hạn</label>
-  	<div class="col-xs-6">
-  		<?php echo $muon['ngay_hethan'];?>
-  	</div>
-</div>
-<div class="form-group row">
-    <label for="example-tel-input" class="col-xs-4 col-form-label">Đầu sách</label>
-    <div class="col-xs-6">
-      <?php
-      $dausach = DauSach::getInstance()->getDauSach($isbn);
-      echo $dausach['bia'] ;
-      ?>
-    </div>
-</div>
-<div class="form-group row">
-    <label for="example-tel-input" class="col-xs-4 col-form-label">&nbsp; </label>
-    <div class="col-xs-3">
-      <a class="btn btn-info btn-sm pull-right" href ="index.php?act=trasach&mcs=<?php echo $ma_cuonsach;?>&isbn=<?php echo $isbn;?>&trasach=1"> Trả cuốn sách này </a>
-    </div>
+  <div class="form-group row">
+    	<label for="example-tel-input" class="col-xs-4 col-form-label">Tên Cuốn sách</label>
+    	<div class="col-xs-6">
+    		<?php echo $muon['bia'] ;?>
+    	</div>
+  </div>
+  <div class="form-group row">
+    	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày mượn</label>
+    	<div class="col-xs-6">
+    		<?php echo $muon['ngaygio_muon'] ;?>
+    	</div>
+  </div>
+  <div class="form-group row">
+    	<label for="example-tel-input" class="col-xs-4 col-form-label">Ngày hết hạn</label>
+    	<div class="col-xs-6">
+    		<?php echo $muon['ngay_hethan'];?>
+    	</div>
+  </div>
+  <div class="form-group row">
+      <label for="example-tel-input" class="col-xs-4 col-form-label">Đầu sách</label>
+      <div class="col-xs-6">
+        <?php
+        $dausach = DauSach::getInstance()->getDauSach($isbn);
+        echo $dausach['bia'] ;
+        ?>
+      </div>
+  </div>
+  <div class="form-group row">
+      <label for="example-tel-input" class="col-xs-4 col-form-label">&nbsp; </label>
+      <div class="col-xs-3">
+        <a class="btn btn-info btn-sm pull-right" href ="index.php?act=trasach&mcs=<?php echo $ma_cuonsach;?>&isbn=<?php echo $isbn;?>&trasach=1"> Trả cuốn sách này </a>
+      </div>
 
+  </div>
 </div>
